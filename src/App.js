f03622b4ef8ea2478cc091380d7fea0a2e3525be
Matleena Autio <c0auma00@students.oamk.fi>
2021-09-27 20:20:09 +0300

    news

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import Detail from './Detail'
+
+const URL = 'https://newsapi.org/v2'
+const APIKEY = 'api key here';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+    const criteria = 'top-headlines?country=us&category=business';
+    const address = URL + '/' + criteria + '&apikey=' + APIKEY;
+
+    axios.get(address)
+      .then((response) => {
+        setError(null);
+        setIsLoaded(true);
+        setItems(response.data.articles);
+      }).catch(error => {
+        alert(error);
+      });
+  }, [])
+
+  function close() {
+    setSelectedItem(null);
+  }
+
+  if (selectedItem != null) {
+    return <Detail
+    title={selectedItem.title}
+    image={selectedItem.urlToImage}
+    description={selectedItem.description}
+    close={close}
+    />;
+  } 
+    else if (error) {
+      return <p>{error.message}</p>
+  }
+    else if (!isLoaded) {
+      return <p>Loading...</p>;
+  } else {
+    return (
+      <div>
+        {items?.map(item =>(
+          <div key={item.title} onClick={e => setSelectedItem(item)}>
+            <h3>{item.title}</h3>
+            <img src={item.urlToImage}></img>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
